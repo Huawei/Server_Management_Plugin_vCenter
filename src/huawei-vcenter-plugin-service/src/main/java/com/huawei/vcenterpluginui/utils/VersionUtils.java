ab85d74aada861103ed38f18c07162804faff8a6
serverplugin <31431891+serverplugin@users.noreply.github.com>
2018-09-10 10:43:44 +0800

    modify define alarm

--- src/huawei-vcenter-plugin-service/src/main/java/com/huawei/vcenterpluginui/utils/VersionUtils.java
+++ src/huawei-vcenter-plugin-service/src/main/java/com/huawei/vcenterpluginui/utils/VersionUtils.java
@@ -2,7 +2,7 @@ package com.huawei.vcenterpluginui.utils;
 
 public class VersionUtils {
 
-    private static final String NEW_VERSION = "1.4.8";
+    private static final String NEW_VERSION = "1.4.10";
     
 	public static String getVersion(){
 		return NEW_VERSION;
